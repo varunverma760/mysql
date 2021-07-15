9aff0642f6808e94ec85d7e7ed66c93ff1391653
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-07-15 04:23:33 -0700

    Add files via upload

---
+++ sqlfile3.sql
@@ -0,0 +1,6 @@
+-- creating a database and deleting a database
+-- to connect the my sql to internet command:- \connect root@localhost 
+CREATE DATABASE abc;
+SELECT DATABASE(); 
+USE abc; -- using a databse
+DROP DATABASE abc; -- use to deleting a database
